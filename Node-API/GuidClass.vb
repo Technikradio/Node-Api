139956a318879d94ed8c57b952a9efb3e2d3f5e5
Technikradio <technikradio@icloud.com>
2013-11-10 17:13:13 +0100

    rename class1.vb to interface.vb and add some code

--- Node-API/GuidClass.vb
+++ Node-API/GuidClass.vb
@@ -1,3 +1,11 @@
 ﻿Public Class GuidClass
-
+    Public Enum tree As UShort
+        file = 0
+        edit = 1
+        view = 2
+        more = 3
+    End Enum
+    Public menutree As tree 'Where the Menubutton should be
+    Public text As String 'The text of the menubutton
+    Public key As String 'The key of the Button: Note! Each button must have its own key because if there are to button (also from other plugins) it isn´t possible anymore which button is pressed.
 End Class
---
+++ Node-API/interface.vb
@@ -0,0 +1,9 @@
+﻿Public Interface mainInteface
+
+    Sub wasContentAdded(ByVal Value As Decimal, ByVal index As Integer, ByVal Description As String, ByVal CostCenter As String, ByVal creationDate As String) 'This will be called if a row was added
+    Sub OnLoading() 'This will be called at the startup of node
+    Sub LogAdd(ByVal log As String) 'This will be called if something is add to the log
+    Property wasLoaded As Boolean 'This should contains if the OnLoading Sub was called or not
+    Property GuidClasses As List(Of GuidClass) 'This will only be called once to inizilize the menubuttons
+
+End Interface
