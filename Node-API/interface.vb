980878dc6f449f53620426a064c658f254c2376b
Doralitze <leon.dietrich@carolinum.de>
2013-11-10 17:50:20 +0100

    Update interface.vb
    Do some commiting

--- Node-API/interface.vb
+++ Node-API/interface.vb
@@ -5,6 +5,6 @@
     Sub LogAdd(ByVal log As String) 'This will be called if something is add to the log
     Property wasLoaded As Boolean 'This should contains if the OnLoading Sub was called or not
     Property GuidClasses As List(Of GuidClass) 'This will only be called once to inizilize the menubuttons
-    Sub wasButtonClicked(ByVal key As String)
+    Sub wasButtonClicked(ByVal key As String) 'This will be called if an MenuButton of this or an other plugin is clicked. The key value is to keep apart of the different Buttons.
 
 End Interface
