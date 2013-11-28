e1d7bac61dd9caa06d4182ffd37fd7bf14074fcc
Technikradio <technikradio@icloud.com>
2013-11-28 20:45:39 +0100

    Give the plugins a voice

--- Node-API/interface.vb
+++ Node-API/interface.vb
@@ -9,5 +9,6 @@
     Property SendToAdd As List(Of analogBorder) 'This contails all rows the plugin add Note: this will only be called every 0.1 seconds
     Property ProcessingSendToAdd As Boolean 'This is true if the client is working on the requests. Don´t add something if it is the case, else it would be deleated!
     Property ElapsedTime As UInt16 'This is the time the client is running stable. it will be updated while the client is processing the rows to add
+    Property MessagesToLog As List(Of String) 'Things which will be shown at the log
 
 End Interface
